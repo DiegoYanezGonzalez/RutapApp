dd0f59ac5517303f4116ca096d11ccb5f8ad6c6e
diego <diego.y8@gmail.com>
2021-04-26 14:35:21 -0400

    CommitFixContextPermissions

--- App.tsx
+++ App.tsx
@@ -2,11 +2,25 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Navigator } from './src/navigation/navigator';
+import { PermissionsProvider } from './src/context/PermussionsContext';
+
+
+const AppState = ({children}:any) =>{
+  return(
+
+    <PermissionsProvider>
+      {children}
+    </PermissionsProvider>
+
+  )
+}
 
  const App = () => {
   return (
     <NavigationContainer>
-       <Navigator/>
+      <AppState>
+         <Navigator/>
+      </AppState>
     </NavigationContainer>
   )
 }
--- src/context/PermussionsContext.tsx
+++ src/context/PermussionsContext.tsx
@@ -1,5 +1,5 @@
 
-import { useState } from 'react';
+import React,{ useState } from 'react';
 import {createContext} from 'react';
 import {PermissionStatus} from 'react-native-permissions';
 
