ce1c72455c5e9bd57cfdb1258ce1c9e6e850b143
diego <diego.y8@gmail.com>
2021-04-26 18:02:39 -0400

    FirtsMapWithAndroid

--- src/pages/MapScreen.tsx
+++ src/pages/MapScreen.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text } from 'react-native';
+import MapView from 'react-native-maps';
 
 const MapScreen = () => {
     return (
-        <View>
-            <Text>Maps Screen</Text>
+        <View style={{flex:1}}>
+            <MapView
+          style={{flex:1}}
+          initialRegion={{
+         latitude: 37.78825,
+         longitude: -122.4324,
+         latitudeDelta: 0.015,
+         longitudeDelta: 0.0121,
+       }}
+     >
+     </MapView>
         </View>
     )
 }
