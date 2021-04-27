7ddbe187b54e27848c75f64b27b972aefc5d702e
diego <diego.y8@gmail.com>
2021-04-27 15:27:25 -0400

    MapsScreen

--- src/pages/MapScreen.tsx
+++ src/pages/MapScreen.tsx
@@ -1,20 +1,12 @@
 import React from 'react'
 import { View, Text } from 'react-native';
-import MapView from 'react-native-maps';
+import { Map } from '../components/Map';
+
 
 const MapScreen = () => {
     return (
         <View style={{flex:1}}>
-            <MapView
-          style={{flex:1}}
-          initialRegion={{
-         latitude: 37.78825,
-         longitude: -122.4324,
-         latitudeDelta: 0.015,
-         longitudeDelta: 0.0121,
-       }}
-     >
-     </MapView>
+           <Map/>
         </View>
     )
 }
